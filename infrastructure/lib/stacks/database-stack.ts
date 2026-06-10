ac101debe875401edc46877bb3c2b702bcd54a35
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-10 08:58:53 -0500

    Setup AWS Secret Manager to store Amazon Aurora DB credentials

--- infrastructure/lib/stacks/database-stack.ts
+++ infrastructure/lib/stacks/database-stack.ts
@@ -7,5 +7,19 @@ export class DatabaseStack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.dbSecret = new secretsmanager.Secret(this, "DBSecret", {
+      secretName: "w-rs-db-credentials",
+      description: "Aurora PostgreSQL DB credentials",
+
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: "wrsadmin" }),
+        generateStringKey: "password",
+        excludeCharacters: ' %+~`#$&*()|[]{}:;<>?!\'/@"\\',
+        passwordLength: 32
+      },
+
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
   }
 }
