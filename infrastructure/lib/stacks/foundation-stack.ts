1bb8f97c3907762c3ef77923e576e06f37cb4581
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-10 08:54:25 -0500

    Create User Pool to store users with Cognito, create S3 buckets to deploy app frontend and store user receipts, and create cloudfront distribution to cache content

--- infrastructure/lib/stacks/foundation-stack.ts
+++ infrastructure/lib/stacks/foundation-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
 
 export class FoundationStack extends cdk.Stack {
@@ -12,5 +14,159 @@ export class FoundationStack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Cognito User Pool -> store all data about users
+    this.userPool = new cognito.UserPool(this, "UserPool", {
+      userPoolName: "w-rs-userpool",
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true
+      },
+      autoVerify: {
+        email: true
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true
+        }
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireUppercase: true,
+        requireLowercase: true,
+        requireDigits: true,
+        requireSymbols: true
+      },
+      mfa: cognito.Mfa.OFF,
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+
+      // Delete UserPool when run `cdk destroy`
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    // Cognito App Client -> consumer of the User Pool
+    this.userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool: this.userPool,
+      userPoolClientName: "w-rs-userpool-webclient",
+      generateSecret: false,
+      authFlows: {
+        userSrp: true
+      },
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true
+        },
+        scopes: [
+          cognito.OAuthScope.OPENID,
+          cognito.OAuthScope.EMAIL,
+          cognito.OAuthScope.PROFILE
+        ],
+        callbackUrls: ["http://localhost:5173"],
+        logoutUrls: ["http://localhost:5173"]
+      },
+      idTokenValidity: cdk.Duration.hours(1),
+      accessTokenValidity: cdk.Duration.hours(1),
+      refreshTokenValidity: cdk.Duration.days(30),
+    });
+
+    this.userPool.addDomain("UserPoolDomain", {
+      cognitoDomain: {
+        domainPrefix: "w-rs-auth"
+      }
+    });
+
+    // Frontend S3 Bucket -> host React app
+    this.frontendBucket = new s3.Bucket(this, "FrontendBucket", {
+      bucketName: `w-rs-bk-frontend-${this.account}`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      versioned: true,
+
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    // Receipts S3 Bucket -> store user receipts
+    this.receiptsBucket = new s3.Bucket(this, "ReceiptsBucket", {
+      bucketName: `w-rs-bk-receipts-${this.account}`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      versioned: true,
+
+      cors: [{
+        allowedOrigins: ["http://localhost:5173"],
+        allowedMethods: [
+          s3.HttpMethods.GET,
+          s3.HttpMethods.PUT,
+          s3.HttpMethods.HEAD
+        ],
+        allowedHeaders: ["*"],
+        exposedHeaders: ["ETag"],
+        maxAge: 3000 // 3sec
+      }],
+
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    // CloudFront distribution -> make frontend bucket accessible and provides cache
+    const distribution = new cloudfront.Distribution(this, "FrontendDistribution", {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(this.frontendBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        compress: true
+      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: "/index.html",
+          ttl: cdk.Duration.seconds(0)
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: "/index.html",
+          ttl: cdk.Duration.seconds(0)
+        }
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      defaultRootObject: "index.html",
+      httpVersion: cloudfront.HttpVersion.HTTP2
+    });
+
+    // Outputs -> logs of all resources created to retrieve URLs or IDs
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: this.userPool.userPoolId,
+      description: 'Cognito User Pool ID',
+      exportName: 'ReceiptSystem-UserPoolId',
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: this.userPoolClient.userPoolClientId,
+      description: 'Cognito App Client ID',
+      exportName: 'ReceiptSystem-UserPoolClientId',
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontUrl', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'CloudFront URL',
+      exportName: 'ReceiptSystem-CloudFrontUrl',
+    });
+
+    new cdk.CfnOutput(this, 'FrontendBucketName', {
+      value: this.frontendBucket.bucketName,
+      description: 'S3 bucket to deploy the React App',
+      exportName: 'ReceiptSystem-FrontendBucketName',
+    });
+
+    new cdk.CfnOutput(this, 'ReceiptsBucketName', {
+      value: this.receiptsBucket.bucketName,
+      description: 'S3 bucket where receipts are uploaded',
+      exportName: 'ReceiptSystem-ReceiptsBucketName',
+    });
+
   }
 }
