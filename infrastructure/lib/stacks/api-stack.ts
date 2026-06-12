8e587d945af55c52b4fef7915cd859eb6e3b3b41
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-12 09:36:48 -0500

    Refactor api stack extracting all code and just setup to start consuming it from there

--- infrastructure/lib/stacks/api-stack.ts
+++ infrastructure/lib/stacks/api-stack.ts
@@ -1,29 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
-import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as rds from 'aws-cdk-lib/aws-rds';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
-interface ApiStackProps extends cdk.StackProps {
-  userPool: cognito.UserPool;
-  userPoolClient: cognito.UserPoolClient;
-  receiptsBucket: s3.Bucket;
-  dbSecret: secretsmanager.ISecret;
-  dbInstance: rds.DatabaseInstance;
-  lambdaSecurityGroup: ec2.SecurityGroup;
-}
+
+import { ApiStackProps } from "../../interfaces";
+import { HttpApiConstruct, ReceiptApiLambda, UploadLambda } from "../constructs";
 
 export class ApiStack extends cdk.Stack {
+  public readonly apiURL: string;
+
   constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props);
 
-    // Avoid TS "unused variables" error
-    void props.userPool;
-    void props.userPoolClient;
-    void props.receiptsBucket;
-    void props.dbSecret;
-    void props.dbInstance;
-    void props.lambdaSecurityGroup;
+    const uploadLambda = new UploadLambda(this, "UploadLambdaURL", {
+      bucket: props.receiptsBucket,
+      securityGroup: props.lambdaSecurityGroup
+    });
+
+    const receiptLambda = new ReceiptApiLambda(this, "ReceiptAPILambda", {
+      dbSecret: props.dbSecret,
+      dbInstance: props.dbInstance,
+      securityGroup: props.lambdaSecurityGroup
+    });
+
+    const api = new HttpApiConstruct(this, "HttpApi", {
+      userPool: props.userPool,
+      userPoolClient: props.userPoolClient,
+      uploadLambda: uploadLambda.fn,
+      receiptLambda: receiptLambda.fn
+    });
+
+    this.apiURL = api.apiUrl;
   }
 }
